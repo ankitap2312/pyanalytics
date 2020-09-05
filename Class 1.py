4bd642138b998049b0389de5448d0b25581b934a
ankitap2312 <ankitap2312@gmail.com>
2020-09-05 22:24:10 +0530

    Create Class 1.py

---
+++ Class 1.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 20:12:02 2020
+
+@author: Ankita
+"""
+## change end and sep
+# print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+print("Ankita","Panigrahy",end=' ')
+print("Ankita","Panigrahy",sep='*')
+
+# help() gives the description 
+help(print)
+
+help("OPERATORS")
+
+A=10
+print(A)
+
+#to run cmd command in spyder
+# !python --version
+
+county=input("Your country: ")
+
+print(county)
+
+type(A)
+
+#converting data types
+Num = input("Number: ")
+
+type(Num)
+
+int(Num)
+
+type(Num)
+
+#different types of print
+print("I live in ",A)
+print("I live in {0}".format(county))
+
+fl=1.11
+
+f2 = int(fl)
+
+#boolean
+x=0
+y=1
+
+z= x and y
+
+A=False
+X=not(A)
+
+fname="Ankita"
+lname="panigrahy"
+name=fname+" "+lname
+name
+
+lname = lname.capitalize()
+lname.upper()
+
+Lname = lname.lower()
+print(Lname)
+
+Lname=lname.rjust(12)
+print(Lname)
+
+Lname=lname.center(14)
+print(Lname)
+
+nam = 'Ankita Panigrahy 5'
+nam
+
+nam=nam.replace('5', '7')
+nam
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
